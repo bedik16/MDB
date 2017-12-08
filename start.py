b4cb7fa7bcac70c81b8d5c73edbf5a2d0adf8f45
bedik16 <berkdikici93@gmail.com>
2017-12-08 16:38:38 +0100

    ScreenSaver video added

--- start.py
+++ start.py
@@ -4,8 +4,16 @@ from kivy.animation import Animation
 from kivy.clock import Clock
 from kivy.uix.image import Image
 from kivy.uix.image import AsyncImage
+from kivy.uix.video import Video
+from kivy.properties import ObjectProperty
 
 class Start(Screen):
+    videoPlayer  = ObjectProperty()
+
+    def on_enter(self):
+        self.videoPlayer.source = 'carpediem.avi'
+        self.videoPlayer.play = True
+        
     def on_touch_down(self, touch):
         self.manager.transition = SlideTransition(direction = "left")
         self.manager.current = "login"
